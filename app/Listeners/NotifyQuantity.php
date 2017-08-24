162b985f93c4914b7ed4ba1e0cd1ec83fed6d522
cedrickfb <cedf.bou@gmail.com>
2017-08-24 18:01:06 -0400

    dafuck

--- app/Listeners/NotifyQuantity.php
+++ app/Listeners/NotifyQuantity.php
@@ -4,6 +4,7 @@ namespace App\Listeners;
 
 
 use App\Category;
+use App\Event;
 use App\Notification;
 /*
 use Illuminate\Database\Eloquent;
@@ -41,20 +42,19 @@ class NotifyQuantity
                 $notif = new Notification([
                    'title' => 'La quantitée est en dessous du minimum',
                    'text' => 'Il reste actuellement : ' .  $event->product['qty'] . ' '
-                       . $event->product['name'] . '(Categorie : '
-                       . $parent[0]['attributes']['name'] .  ') et le minumum est : '.  $event->product['min_qty'],
+                       . $event->product['name'] . ' et le minumum est : '.  $event->product['min_qty'],
                    'viewed' => false,
                    'created_at' => Carbon::now(),
                    'updated_at' => Carbon::now()]);
                $notif->save();
+               \event(new New)
             }else{
                 if($event->product['qty'] >= $event->product['max_qty']){
                     $parent = Category::select('name')->whereRaw('id = ' . $event->product['category_id'])->get();
                     $notif = new Notification([
                         'title' => 'La quantitée est au dessus du maximum',
                         'text' => 'Il y as actuellement : ' .  $event->product['qty'] . ' '
-                            . $event->product['name'] . '(Categorie : '
-                            . $parent[0]['attributes']['name'] .  ') et le maximum est : '.  $event->product['max_qty'],
+                            . $event->product['name'] . ' et le maximum est : '.  $event->product['max_qty'],
                         'viewed' => false,
                         'created_at' => Carbon::now(),
                         'updated_at' => Carbon::now()]);
